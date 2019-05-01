cf4f2dd91f6deedb0acc68cd700ec99b0d5e128c
MiguelJiRo <mn372920@gmail.com>
2019-05-01 20:33:28 +0200

    + mapa con bloques de wario

--- javascript/game.js
+++ javascript/game.js
@@ -1,7 +1,7 @@
 window.addEventListener('load',function()
 {
 	var Q = Quintus({audioSupported:['mp3','ogg']})
-			.include('Sprites, Scenes, Input, 2D, Anim, Touch, UI, TMX, Audio')
+			.include('Sprites, Scenes, Input, 2D, Anim, Touch, UI, TMX, Audio')		
 		    .setup({ 
 		    	width: 320, 
 		    	height: 480 
@@ -11,12 +11,11 @@ window.addEventListener('load',function()
 		    .enableSound();	
 	
 	add_Level(Q);
-	//add_Mario(Q);
+	add_Mario(Q);
 	
-	//, mario_small.png, mario_small.json
-	Q.loadTMX('level.tmx', function()
+	Q.loadTMX('level0001.tmx, mario_small.png, mario_small.json', function()
 	{		
-		//Q.compileSheets('mario_small.png', 'mario_small.json');	
+		Q.compileSheets('mario_small.png', 'mario_small.json');	
 		Q.stageScene('level1');
 	});
 });
--- javascript/level1.js
+++ javascript/level1.js
@@ -3,10 +3,10 @@ function add_Level(Q){
 	//crea una nueva scene llamada level1
 	Q.scene('level1', function(stage)
 	{
-		Q.stageTMX('level.tmx',stage);	
-		//var player = stage.insert(new Q.Mario());
+		Q.stageTMX('level0001.tmx',stage); 
+		var player = stage.insert(new Q.Mario());
 
-		/*stage.add('viewport')
+		stage.add('viewport')
 			.follow(player, 
 				{
 					x: true, 
@@ -16,6 +16,5 @@ function add_Level(Q){
 					minY: 125, 
 					maxY: 500
 				});
-				*/
 	});
-}
+};
