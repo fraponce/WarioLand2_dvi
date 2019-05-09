a08393567fd6c103ab34b14109a57066911a07e0
fraponce <fraponce@ucm.es>
2019-05-09 15:27:06 +0200

    separacion de enemy

---
+++ javascript/enemy1.js
@@ -0,0 +1,78 @@
+
+function add_enemy1(Q){
+    Q.Sprite.extend('enemy1',{
+        init: function(p){
+            this._super(p,{
+                sprite: 'anim_enemy1',
+                sheet: 'enemy1',
+                vx: 50,
+                lado: 1,
+                points: [[-6,-6],[6,-6],[6,6],[-6,6]],
+                hamuerto:false
+            });
+            this.add('2d, aiBounce, animation, defaultEnemy');
+            this.on('die');
+
+
+
+            this.on("bump.top", function(collision) {
+                if (collision.obj.isA("Wario"))
+                  if(collision.obj.p.culetazo){
+                    this.play("die");
+                    this.p.vy = -300;
+                    this.die();
+                }
+          	});
+          	this.on("bump.left", function(collision) {
+                if (collision.obj.isA("Wario"))
+                  if(this.p.lado == 0){
+                    collision.obj.play("die");
+                    collision.obj.die();
+                  }else if (collision.obj.p.placando){
+                    this.play("die");
+                    this.p.vy = -300;
+                    this.die();
+            	  }
+          	});
+          	this.on("bump.right", function(collision) {
+                if (collision.obj.isA("Wario"))
+                  if(this.p.lado == 1){
+                    collision.obj.play("die");
+                    collision.obj.die();
+                  }else if (collision.obj.p.placando){
+                    this.play("die");
+                    this.p.vy = -300;
+                    this.die();
+            }
+          	});
+        },
+        step: function(dt) 
+        {
+            if(this.p.vx > 0 ) 
+                this.p.lado=1; //Derecha
+            else if(this.p.vx<0) 
+                this.p.lado = 0; //Izquierda
+
+
+            if(!this.p.hamuerto)
+                 if(this.p.vx > 0 ) 
+                this.play('walkL'); //Izquierda
+            else if(this.p.vx<0) 
+                this.play('walkR'); //Derecha
+            else
+                this.play('die');           
+        },
+        die: function(dt)
+        {
+            this.p.hamuerto = true;             
+            this.destroy();
+        }
+    });
+   
+    Q.animations('anim_enemy1',{
+        walkL:{frames:[0,1,2], rate: 1/6, flip: "x", loop: true},
+        walkR:{frames:[0,1,2], rate: 1/6, flip: false, loop: true},
+        die:{frames:[3],loop: false}
+    });
+       
+}
