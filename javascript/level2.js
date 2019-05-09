039dc38e98acbc784d3835da687aa40d886b5d7a
fraponce <fraponce@ucm.es>
2019-05-09 15:40:36 +0200

    Update level2.js

--- javascript/level2.js
+++ javascript/level2.js
@@ -30,7 +30,7 @@ function add_Level2(Q){
 		var bigcoin008b = stage.insert(new Q.bigcoin({x: 1200,y: 252}));
 		var bigcoin009b = stage.insert(new Q.bigcoin({x: 1310,y: 316}));
 
-		var enemy001 =  stage.insert(new Q.enemy002({x: 1310,y: 316}));
+		var enemy001 =  stage.insert(new Q.enemy2({x: 1310,y: 316}));
 
 		//Puertas
 		//Puerta verde
