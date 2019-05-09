e65333f27696ee7a84e781eaadd49485e09b0f6d
fraponce <fraponce@ucm.es>
2019-05-09 15:31:32 +0200

    Update enemy2.js

--- javascript/enemy2.js
+++ javascript/enemy2.js
@@ -1,81 +1,3 @@
-
-function add_enemy1(Q){
-    Q.Sprite.extend('enemy1',{
-        init: function(p){
-            this._super(p,{
-                sprite: 'anim_enemy1',
-                sheet: 'enemy1',
-                vx: 50,
-                lado: 1,
-                points: [[-6,-6],[6,-6],[6,6],[-6,6]],
-                hamuerto:false
-            });
-            this.add('2d, aiBounce, animation, defaultEnemy');
-            this.on('die');
-
-
-
-            this.on("bump.top", function(collision) {
-                if (collision.obj.isA("Wario"))
-                  if(collision.obj.p.culetazo){
-                    this.play("die");
-                    this.p.vy = -300;
-                    this.die();
-                }
-          	});
-          	this.on("bump.left", function(collision) {
-                if (collision.obj.isA("Wario"))
-                  if(this.p.lado == 0){
-                    collision.obj.play("die");
-                    collision.obj.die();
-                  }else if (collision.obj.p.placando){
-                    this.play("die");
-                    this.p.vy = -300;
-                    this.die();
-            	  }
-          	});
-          	this.on("bump.right", function(collision) {
-                if (collision.obj.isA("Wario"))
-                  if(this.p.lado == 1){
-                    collision.obj.play("die");
-                    collision.obj.die();
-                  }else if (collision.obj.p.placando){
-                    this.play("die");
-                    this.p.vy = -300;
-                    this.die();
-            }
-          	});
-        },
-        step: function(dt) 
-        {
-            if(this.p.vx > 0 ) 
-                this.p.lado=1; //Derecha
-            else if(this.p.vx<0) 
-                this.p.lado = 0; //Izquierda
-
-
-            if(!this.p.hamuerto)
-                 if(this.p.vx > 0 ) 
-                this.play('walkL'); //Izquierda
-            else if(this.p.vx<0) 
-                this.play('walkR'); //Derecha
-            else
-                this.play('die');           
-        },
-        die: function(dt)
-        {
-            this.p.hamuerto = true;             
-            this.destroy();
-        }
-    });
-   
-    Q.animations('anim_enemy1',{
-        walkL:{frames:[0,1,2], rate: 1/6, flip: "x", loop: true},
-        walkR:{frames:[0,1,2], rate: 1/6, flip: false, loop: true},
-        die:{frames:[3],loop: false}
-    });
-       
-}
 function add_enemy2(Q){
      Q.Sprite.extend('enemy2',{
         init: function(p){
@@ -115,17 +37,11 @@ function add_enemy2(Q){
         },
         shoot: function(dt) 
         {
-            if(this.p.vx > 0 ) 
-                this.p.lado=1; //Derecha
-            else if(this.p.vx<0) 
-                this.p.lado = 0; //Izquierda
-
-
             if(!this.p.hamuerto)
-                 if(this.p.vx > 0 ) 
-                this.play('walkL'); //Izquierda
-            else if(this.p.vx<0) 
-                this.play('walkR'); //Derecha
+                 if(this.p.lado = 0 ) 
+                    this.play('shootL'); //Izquierda
+            else if(this.p.lado = 1) 
+                this.play('shootR'); //Derecha
             else
                 this.play('die');           
         },
@@ -136,8 +52,10 @@ function add_enemy2(Q){
         }
 
         Q.animations('anim_enemy2',{
-        walkL:{frames:[0,1,2], rate: 1/6, flip: false, loop: true},
-        walkR:{frames:[3], rate: 1/6, flip: "x", loop: true},
+        shootL:{frames:[0,1,2], rate: 1/6, flip: false, loop: true},
+        chargeL:{frames:[3], rate: 1/6, flip: "x", loop: true},
+        shootR:{frames:[0,1,2], rate: 1/6, flip: "x", loop: true},
+        chargeR:{frames:[3], rate: 1/6, flip: false, loop: true},
         die:{frames:[4],loop: false}
     });
 
