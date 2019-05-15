28c31e49d9fbece5f39c123f4fa18472d1c8607e
MiguelJiRo <mn372920@gmail.com>
2019-05-15 16:17:45 +0200

    + Añadido sonido en la Intro

--- javascript/pantallaintro.js
+++ javascript/pantallaintro.js
@@ -1,14 +1,19 @@
 function add_PantallaIntro(Q)
 {
+    Q.load('Intro.mp3');
+    
 	Q.scene('pantallaIntro',function(stage) 
 	{
 		var intro = stage.insert(new Q.Intro30sec());
 		var confirm = true;
+        Q.audio.stop();
+        Q.audio.play('Intro.mp3');
 
 		Q.input.on('confirm', this, function() 
 		{
             if (confirm) 
-            {            
+            { 
+                Q.audio.stop();           
                 Q.clearStages();
                 Q.stageScene('pantallaMenu');
                 confirm = false;
